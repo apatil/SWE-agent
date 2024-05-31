6dc3c1157a52080282c358d9c9c1f2b721f59fa7
Anand Patil <anand@continual.ai>
2024-05-31 15:26:35 -0400

    small

--- sweagent/__init__.py
+++ sweagent/__init__.py
@@ -5,9 +5,9 @@ __version__ = "0.5.0"
 from pathlib import Path
 import os, sys
 
-PACKAGE_DIR = Path(__file__).resolve().parent
+PACKAGE_DIR = Path(os.path.join(sys.prefix, "src/SWE-Agent/sweagent")).resolve()
 assert PACKAGE_DIR.is_dir()
-REPO_ROOT = os.path.join(sys.prefix, "src/SWE-Agent")
+REPO_ROOT = PACKAGE_DIR.parent
 assert REPO_ROOT.is_dir()
 CONFIG_DIR = PACKAGE_DIR.parent / "config"
 assert CONFIG_DIR.is_dir()
